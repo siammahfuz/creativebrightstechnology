4316e9e431cda704fb11d9ae5ee9eb15f07511e6
MD Mahfuzur Rahman Shium <ksiam3409@gmail.com>
2023-06-03 04:24:47 +0600

    Add files via upload

---
+++ Readme.md
@@ -0,0 +1,17 @@
+# Responsive Cristiano Ronaldo Website
+
+## [See Live Preview](https://cr7-ronaldo.netlify.app/)
+
+###  Responsive CR7 Website
+
+- CR7 website built using HTML, CSS, JAVASCRIPT & jQuery.
+- Contains interactive stats linegraph.
+- Includes beautiful images with played club details with stats.
+- Includes video on top goals of CR7.
+- Developed with the Mobile Responsive methodology.
+- Compatible with all devices of different screen sizes and with a beautiful and pleasant user interface.
+
+
+![cr7-website](images/preview.png)
+
+I don't own the photos used in this site. Cristiano Ronaldo (CR7) has all images right reserved
---
+++ javascript/main.js
@@ -0,0 +1,375 @@
+$(document).ready(function () {
+  $(".btn").click(function () {
+    $(".items").toggleClass("show");
+    $("ul li").toggleClass("hide");
+  });
+});
+const scrollBtn = document.querySelector(".scrollToTop-btn");
+
+window.addEventListener("scroll", function () {
+  scrollBtn.classList.toggle("active", window.scrollY > 500);
+});
+
+scrollBtn.addEventListener("click", () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
+
+window.Apex = {
+  chart: {
+    foreColor: "#ccc",
+    toolbar: {
+      show: false,
+    },
+  },
+  stroke: {
+    width: 3,
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  tooltip: {
+    theme: "dark",
+  },
+  grid: {
+    borderColor: "#535A6C",
+    xaxis: {
+      lines: {
+        show: true,
+      },
+    },
+  },
+};
+
+var spark1 = {
+  chart: {
+    id: "spark1",
+    group: "sparks",
+    type: "line",
+    height: 100,
+    sparkline: {
+      enabled: true,
+    },
+    dropShadow: {
+      enabled: true,
+      top: 1,
+      left: 1,
+      blur: 2,
+      opacity: 0.2,
+    },
+  },
+  series: [
+    {
+      data: [0, 100],
+    },
+  ],
+  stroke: {
+    curve: "smooth",
+  },
+  markers: {
+    size: 0,
+  },
+  grid: {
+    padding: {
+      top: 20,
+      bottom: 10,
+      left: 110,
+    },
+  },
+  colors: ["#fff"],
+  tooltip: {
+    x: {
+      show: false,
+    },
+    y: {
+      title: {
+        formatter: function formatter(val) {
+          return "";
+        },
+      },
+    },
+  },
+};
+
+var spark2 = {
+  chart: {
+    id: "spark2",
+    group: "sparks",
+    type: "line",
+    height: 100,
+    sparkline: {
+      enabled: true,
+    },
+    dropShadow: {
+      enabled: true,
+      top: 1,
+      left: 1,
+      blur: 2,
+      opacity: 0.2,
+    },
+  },
+  series: [
+    {
+      data: [0, 100],
+    },
+  ],
+  stroke: {
+    curve: "smooth",
+  },
+  grid: {
+    padding: {
+      top: 20,
+      bottom: 10,
+      left: 110,
+    },
+  },
+  markers: {
+    size: 0,
+  },
+  colors: ["#fff"],
+  tooltip: {
+    x: {
+      show: false,
+    },
+    y: {
+      title: {
+        formatter: function formatter(val) {
+          return "";
+        },
+      },
+    },
+  },
+};
+
+var spark3 = {
+  chart: {
+    id: "spark3",
+    group: "sparks",
+    type: "line",
+    height: 100,
+    sparkline: {
+      enabled: true,
+    },
+    dropShadow: {
+      enabled: true,
+      top: 1,
+      left: 1,
+      blur: 2,
+      opacity: 0.2,
+    },
+  },
+  series: [
+    {
+      data: [0, 100],
+    },
+  ],
+  stroke: {
+    curve: "smooth",
+  },
+  markers: {
+    size: 0,
+  },
+  grid: {
+    padding: {
+      top: 20,
+      bottom: 10,
+      left: 110,
+    },
+  },
+  colors: ["#fff"],
+  xaxis: {
+    crosshairs: {
+      width: 1,
+    },
+  },
+  tooltip: {
+    x: {
+      show: false,
+    },
+    y: {
+      title: {
+        formatter: function formatter(val) {
+          return "";
+        },
+      },
+    },
+  },
+};
+
+var spark4 = {
+  chart: {
+    id: "spark4",
+    group: "sparks",
+    type: "line",
+    height: 100,
+    sparkline: {
+      enabled: true,
+    },
+    dropShadow: {
+      enabled: true,
+      top: 1,
+      left: 1,
+      blur: 2,
+      opacity: 0.2,
+    },
+  },
+  series: [
+    {
+      data: [0, 100],
+    },
+  ],
+  stroke: {
+    curve: "smooth",
+  },
+  markers: {
+    size: 0,
+  },
+  grid: {
+    padding: {
+      top: 20,
+      bottom: 10,
+      left: 110,
+    },
+  },
+  colors: ["#fff"],
+  xaxis: {
+    crosshairs: {
+      width: 1,
+    },
+  },
+  tooltip: {
+    x: {
+      show: false,
+    },
+    y: {
+      title: {
+        formatter: function formatter(val) {
+          return "";
+        },
+      },
+    },
+  },
+};
+
+new ApexCharts(document.querySelector("#spark1"), spark1).render();
+new ApexCharts(document.querySelector("#spark2"), spark2).render();
+new ApexCharts(document.querySelector("#spark3"), spark3).render();
+new ApexCharts(document.querySelector("#spark4"), spark4).render();
+
+var optionsLine = {
+  chart: {
+    height: 328,
+    type: "line",
+    zoom: {
+      enabled: false,
+    },
+    dropShadow: {
+      enabled: true,
+      top: 3,
+      left: 2,
+      blur: 4,
+      opacity: 1,
+    },
+  },
+  stroke: {
+    curve: "smooth",
+    width: 2,
+  },
+  colors: ["#00ff40", "#ff0040"],
+  series: [
+    {
+      name: "Goals",
+      data: [
+        5, 1, 13, 15, 25, 34, 35, 30, 48, 60, 63, 69, 61, 57, 55, 53, 49, 39,
+        44, 45,
+      ],
+    },
+    {
+      name: "Assist",
+      data: [
+        4, 2, 14, 11, 10, 11, 12, 5, 18, 18, 13, 15, 20, 17, 16, 12, 13, 4, 7,
+        6,
+      ],
+    },
+  ],
+  title: {
+    text: "Goal Contribution (Calendar Year)",
+    align: "left",
+    offsetY: 25,
+    offsetX: 20,
+  },
+  subtitle: {
+    text: "Statistics",
+    offsetY: 55,
+    offsetX: 20,
+  },
+  markers: {
+    size: 6,
+    strokeWidth: 0,
+    hover: {
+      size: 9,
+    },
+  },
+  grid: {
+    show: true,
+    padding: {
+      bottom: 0,
+    },
+  },
+  labels: [
+    "2002",
+    "2003",
+    "2004",
+    "2005",
+    "2006",
+    "2007",
+    "2008",
+    "2009",
+    "2010",
+    "2011",
+    "2012",
+    "2013",
+    "2014",
+    "2015",
+    "2016",
+    "2017",
+    "2018",
+    "2019",
+    "2020",
+    "2021",
+  ],
+  xaxis: {
+    tooltip: {
+      enabled: false,
+    },
+  },
+  legend: {
+    position: "top",
+    horizontalAlign: "right",
+    offsetY: -20,
+  },
+};
+
+var chartLine = new ApexCharts(
+  document.querySelector("#line-adwords"),
+  optionsLine
+);
+chartLine.render();
+
+(() => {
+  const btn = document.getElementById("fusionexport-btn");
+  btn.addEventListener("click", async function () {
+    const endPoint =
+      "https://www.fusioncharts.com/demos/dashboards/fusionexport-apexcharts/api/export-dashboard";
+    const information = {
+      dashboardName: "dark",
+    };
+
+    this.setAttribute("disabled", true);
+    const { data } = await axios.post(endPoint, information, {
+      responseType: "blob",
+    });
+    await download(data, "apexCharts-dark-dashboard.pdf", "application/pdf");
+    this.removeAttribute("disabled");
+  });
+})();
